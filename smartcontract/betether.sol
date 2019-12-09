81fa7fc2bf36b23024d245cfccd5b1b77af95a5c
zulnemekh <bekhbat_zulnemekh@unimedia.co.jp>
2019-12-09 09:27:07 +0800

    abi SM updated

--- smartcontract/betether.sol
+++ smartcontract/betether.sol
@@ -5,13 +5,16 @@ contract EtherBet {
   address payable public owner;
 
   struct Bets {
-    bytes32 par1;  //participate first ID
-    bytes32 par2;  //participate second ID
+    string par1;  //participate first ID
+    string par2;  //participate second ID
     uint closeTIMESTAMP; // close date
     uint8 winner; // 1 => par1: 2 => par2: 3 => draw: 4 => not yet play
     bool isAvailable; // closed: false opened: true
-    bytes32 description;
-    bytes32 betType;
+    bool isPublic;
+    string betCategory;
+    string description;
+    bool canDraw;
+    address payable owner_address;
   }
 
   struct UserBets {
@@ -24,7 +27,7 @@ contract EtherBet {
   }
 
   Bets[] public bets;
-  mapping (uint => bytes32) public disableBets;
+  mapping (uint => string) public disableBets;
   mapping (uint => UserBets[]) public campaigns;
   mapping(uint => mapping(uint => uint)) public betAmountOf; // bet_ID, winner_TYPE, total_amount
   mapping(uint => mapping(address => uint)) public betUserAmountOf; // bet_ID, user_address, amount
@@ -58,13 +61,15 @@ contract EtherBet {
         betUserOf[bet_id][msg.sender] = winner;
         betUserAmountOf[bet_id][msg.sender] += msg.value;
     }
-
-    function createBet(bytes32 par1, bytes32 par2, uint closeTIMESTAMP, bool isAvailable, bytes32 description, bytes32 bType) external onlyOwner {
-        bets.push(Bets(par1, par2, closeTIMESTAMP, 4, isAvailable, description, bType));
+      
+      //"bat-erdene", "suhbat", "1607644800", "true", "sport undesnii boh", "ene bol tailbar", "true"
+    function createBet(string memory par1, string memory par2, uint closeTIMESTAMP, bool canDraw, string memory betCategory, string memory description, bool isPublic) public{
+        bets.push(Bets(par1, par2, closeTIMESTAMP, 4, true, isPublic, betCategory, description, canDraw, msg.sender));
     }
 
-    function agreeBetWinner (uint bet_id, uint8 winner) external onlyOwner{
-
+    function agreeBetWinner (uint bet_id, uint8 winner) external{
+        
+        require(bets[bet_id].owner_address == msg.sender);
         require(bets[bet_id].isAvailable == true);
         require(winner >= 1);
         require(winner <= 3);
@@ -72,7 +77,13 @@ contract EtherBet {
         bets[bet_id].winner = winner;
         sendWinnerPriceToAll(bet_id, winner);
     }
-
+    function cancelBet(uint bet_id) external{
+         require(bets[bet_id].owner_address == msg.sender);
+          for (uint i = 0; i < campaigns[bet_id].length; i++){
+            campaigns[bet_id][i].user_address.transfer(campaigns[bet_id][i].amount);            
+         }
+    }
+    
     function getCampaignLength(uint id) external view returns (uint) {
         return  campaigns[id].length;
     }
@@ -84,8 +95,9 @@ contract EtherBet {
 
     function sendWinnerPriceToAll(uint bet_id, uint8 winner) internal returns (bool) {
         uint total_amount = betAmountOf[bet_id][0] + betAmountOf[bet_id][1] + betAmountOf[bet_id][2];
-        uint winner_precent = total_amount/betAmountOf[bet_id][winner] * 95/100;
-        small_profit += total_amount * 5/100;
+        uint winner_precent = total_amount/betAmountOf[bet_id][winner] * 98/100;
+        uint dev_profit = total_amount * 1/100;
+        uint bet_owner_profit = total_amount * 1/100;
          for (uint i = 0; i < campaigns[bet_id].length; i++){
             if(campaigns[bet_id][i].winner == winner) {
                 campaigns[bet_id][i].user_address.transfer(campaigns[bet_id][i].amount*winner_precent);
@@ -93,24 +105,32 @@ contract EtherBet {
             }
 
          }
-            emit Won(bet_id, winner);
+         bets[bet_id].owner_address.transfer(bet_owner_profit);
+         owner.transfer(dev_profit);
+        
+        emit Won(bet_id, winner);
         return true;
     }
 
-    function getBet(uint bet_id) external view returns (bytes32, bytes32, uint, uint8, bool, bytes32){
+   function getBetInfo(uint bet_id) external view returns (string memory, string memory, uint, string memory, address){
         return (bets[bet_id].par1, bets[bet_id].par2, bets[bet_id].closeTIMESTAMP,
-        bets[bet_id].winner, bets[bet_id].isAvailable, bets[bet_id].description);
+          bets[bet_id].betCategory, bets[bet_id].owner_address);
     }
+    function getBetInfoDetail(uint bet_id) external view returns (uint8, bool, bool, string memory, bool){
+        return (bets[bet_id].winner, bets[bet_id].isAvailable, bets[bet_id].canDraw, bets[bet_id].description, bets[bet_id].isPublic);
+    }
+    
     function setDisableBet(uint bet_id) external onlyOwner{
       disableBets[bet_id]='disable';
     }
-    function getDisableBet(uint bet_id) external view returns (bytes32) {
+    function getDisableBet(uint bet_id) external view returns (string memory) {
         return disableBets[bet_id];
     }
-     function getBetName(uint bet_id) external view returns (bytes32, bytes32){
+     function getBetName(uint bet_id) external view returns (string memory, string memory){
         return (bets[bet_id].par1 ,bets[bet_id].par2);
     }
-
+    
+    
     function getBetLength() external view returns(uint){
          return bets.length;
     }
@@ -133,13 +153,10 @@ contract EtherBet {
     }
 
 
-    function showSmallProfit() external view onlyOwner returns(uint) {
-        return small_profit;
-    }
-    function withdraw() external onlyOwner {
-        owner.transfer(small_profit);
-        small_profit = 0;
-    }
+    // function withdraw() external onlyOwner {
+    //     owner.transfer(small_profit);
+    //     small_profit = 0;
+    // }
 
     event Won(uint indexed bet_id, uint8 winner);
 
